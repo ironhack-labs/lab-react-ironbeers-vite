7ac2277ffff36a889b3d3cddf2a48c842893fbbb
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-08-24 19:07:34 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,17 @@
-function Navbar() {}
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+
+function Navbar() {
+    return (
+
+        <div className="bg-primary text-center">
+            <Link to={'/'} className='nav-link'>
+                <img src="/src/assets/home-icon.png" alt="" />
+            </Link>
+        </div>
+    )
+}
 
 export default Navbar;
