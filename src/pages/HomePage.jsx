80b1063df411217b1260f7c816cfa9809a992c56
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-08-24 21:12:27 +0200

    Done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,53 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function HomePage() {
+
+    return (
+        <>
+
+            <div className="HomePage mb-3">
+                <Card>
+                    <Card.Img variant="top" src="src/assets/beers.png" />
+                    <Card.Body>
+                        <Link to={"/beers"}>
+                            <p>All Beers</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </Link>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className="HomePage mb-3">
+                <Card>
+                    <Card.Img variant="top" src="src/assets/random-beer.png" />
+                    <Card.Body>
+                        <Link to={"/random-beer"}>
+                            <p>Random Beer</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </Link>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className="HomePage mb-3">
+                <Card>
+                    <Card.Img variant="top" src="src/assets/new-beer.png" />
+                    <Card.Body>
+                        <Link to={"/new-beer"}>
+                            <p>New Beer</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </Link>
+                    </Card.Body>
+                </Card>
+            </div>
+
+        </>
+
+
+    )
+
+}
 
 export default HomePage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,43 @@
-function RandomBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function RandomBeersPage() {
+
+    const [beersRandom, setBeersRandom] = useState([])
+
+    useEffect(() => {
+        loadRandomBeer()
+    }, [])
+
+    const loadRandomBeer = () => {
+
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+            .then((beer) => {
+                // console.log(beer.data)
+                setBeersRandom(beer.data);
+            })
+            .catch(err => console.log(err))
+
+    }
+
+    return (
+        <div>
+
+            <img src={beersRandom.image_url} alt="" style={{ width: "100px" }} />
+            <p>{beersRandom.name}</p>
+            <p>{beersRandom.tagline}</p>
+            <p>{beersRandom.first_brewed}</p>
+            <p>{beersRandom.attenuation_level}</p>
+            <p>{beersRandom.description}</p>
+            <p>{beersRandom.contributed_by}</p>
+            <hr />
+
+        </div>
+
+    )
+
+
+}
 
 export default RandomBeersPage;
