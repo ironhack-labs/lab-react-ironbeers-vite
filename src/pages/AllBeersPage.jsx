7a6cb470c15031afabb54771e12103cb47b040e3
samuelemarino04 <samuele12601@gmail.com>
2023-08-25 09:15:30 +0200

    done

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,48 @@
-function AllBeersPage() {}
+import { useEffect } from "react";
+import { useState } from "react";
+import axios, { Axios } from "axios";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+
+function AllBeersPage() {
+
+    const [beer, setBeers] = useState([])
+
+    useEffect(() => {
+        axios.get('https://ih-beers-api2.herokuapp.com/beers')
+
+            .then(response => {
+
+                setBeers(response.data)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            })
+    }, [])
+
+    return (
+        <>
+            {beer.map((elm) => {
+                return (
+                    <>
+                        <Container className="d-flex">
+                            <Link to={`/beers/${elm._id}`}>
+                                <img src={elm.image_url} alt="" className="w-25 h-40" />
+                            </Link>
+                            <div className="d-flex align-items-start flex-column">
+                                <h2>{elm.name}</h2>
+                                <p>{elm.tagline}</p>
+                                <p><strong>Created by: </strong>{elm.contributed_by}</p>
+                            </div>
+
+                        </Container>
+                        <hr />
+                    </>
+                )
+            })}
+        </>
+    )
+}
 
 export default AllBeersPage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,48 @@
-function RandomBeersPage() {}
+import { useEffect } from "react";
+import { useState } from "react";
+import axios, { Axios } from "axios";
+import { useParams } from "react-router-dom"
+import { Container } from "react-bootstrap";
+
+function RandomBeersPage() {
+    const [details, setDetails] = useState(null)
+
+    const { beerId } = useParams()
+
+    useEffect(() => {
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+
+            .then(response => {
+
+                setDetails(response.data)
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            })
+    }, [beerId])
+
+    if (!details) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
+    return (
+        <Container className="d-flex">
+            <img src={details.image_url} alt="beerimg" className="w-25 h-40" />
+            <div className="d-flex align-items-start flex-column">
+                <h3>{details.name}</h3>
+                <p>{details.tagline}</p>
+                <p>{details.first_brewed}</p>
+                <p>{details.attenuation_level}</p>
+                <p>{details.description}</p>
+                <p>{details.contributed_by}</p>
+            </div>
+
+
+        </Container>
+    )
+}
+
 
 export default RandomBeersPage;
