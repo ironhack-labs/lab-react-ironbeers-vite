0e4823c9f850ec8db303e7628cbb71b46294d05b
Elena Ceinos <elenaceinos@hotmail.com>
2023-11-23 22:10:42 +0100

    form not done yet

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,42 @@
-function AllBeersPage() {}
 
-export default AllBeersPage;
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+const URL = "https://ih-beers-api2.herokuapp.com/beers"
+
+const AllBeersPage = () => {
+    const [allBeers, setAllBeers] = useState([])
+
+
+    useEffect(() => {
+        axios
+            .get(URL)
+            .then((response) => {
+                console.log('response data: ', response.data)
+                setAllBeers(response.data)
+            })
+    }, [])
+
+
+    return (
+        <div >
+            <h1> All beers: </h1>
+
+
+            {allBeers.map((e) => (
+
+                <div key={e._id} className="holi">
+                    <Link to={`/beers/${e._id}`}>  <img src={e.image_url} alt="beer" /> </Link>
+                    <h2>{e.name}</h2>
+                    <p> {e.tagline} </p>
+                    <h5> by {e.contributed_by}</h5>
+                </div>
+            )
+            )}
+        </div>
+    )
+
+}
+
+export default AllBeersPage
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,24 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
 
-export default HomePage;
+import "/src/index.css"
+
+const HomePage = () => {
+    return (
+        <nav >
+            <div >
+                <Link to="/beers"><img src="src/assets/beers.png" alt="beer" />
+                    All Beers </Link>
+            </div>
+            <div >
+                <Link to="/random-beer"> <img src="/src/assets/random-beer.png" alt="random " />
+                    Random Beers </Link>
+            </div>
+            <div>
+                <Link to={"/new-beer"}><img src={"/src/assets/new-beer.png"} alt={"new beer"} />
+                    New Beer </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default HomePage
