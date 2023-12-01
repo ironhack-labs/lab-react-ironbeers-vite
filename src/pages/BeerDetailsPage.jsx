e95b60a4e8320b5892bd9db5b8c24582ed9f983c
Lisa Durdun <lisa.durdun@gmail.com>
2023-12-01 18:33:25 +0100

    iterations 1-4

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,34 @@
-function BeerDetailsPage() {}
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers/";
+
+function BeerDetailsPage() {
+  const [beers, setBeers] = useState({});
+  const [data, setData] = useState(false);
+  const { _id } = useParams();
+  useEffect(() => {
+    axios
+      .get(API_URL + _id)
+      .then((resp) => {
+        setBeers(resp.data);
+        setData(true);
+      })
+      .catch((err) => {
+        console.log("Error fetching data from API", err);
+      });
+  }, [_id]);
+
+  return (
+    <div>
+      {data && (
+        <div className="BeerDetailsPage">
+          <img src={beers.image_url} />
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default BeerDetailsPage;
