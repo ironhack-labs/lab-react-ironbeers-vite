d5fbc316b0a22c5f3c81c743dfdd13fad7e7cace
Lisa Durdun <lisa.durdun@gmail.com>
2023-12-04 09:53:10 +0100

    Iteration 5

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -5,26 +5,31 @@ import { useState, useEffect } from "react";
 const API_URL = "https://ih-beers-api2.herokuapp.com/beers/";
 
 function BeerDetailsPage() {
-  const [beers, setBeers] = useState({});
-  const [data, setData] = useState(false);
-  const { _id } = useParams();
+  const [beers, setBeers] = useState([]);
+  const { beerId } = useParams();
   useEffect(() => {
     axios
-      .get(API_URL + _id)
+      .get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
       .then((resp) => {
         setBeers(resp.data);
-        setData(true);
+        console.log("BEER DATA ->: ", resp.data);
       })
       .catch((err) => {
         console.log("Error fetching data from API", err);
       });
-  }, [_id]);
+  }, []);
 
   return (
-    <div>
-      {data && (
-        <div className="BeerDetailsPage">
+    <div key={beers.id}>
+      {beers && (
+        <div className="beer-details-page">
           <img src={beers.image_url} />
+          <h2>{beers.name}</h2>
+          <h3>{beers.tagline}</h3>
+          <p>First brewed: {beers.first_brewed}</p>
+          <p>Attenuation: {beers.attenuation_level}</p>
+          <p>{beers.description}</p>
+          <h3>Contributed by: {beers.contributed_by}</h3>
         </div>
       )}
     </div>
