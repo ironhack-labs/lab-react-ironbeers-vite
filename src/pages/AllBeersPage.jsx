ff2ce5fb1d9b31ebd70494d54d950594c6925972
Eduardo Botelho <eduardo11224b@gmail.com>
2024-02-07 14:38:38 +0000

    all beers page basic structure

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,20 @@
-function AllBeersPage() {}
+import "./AllBeersPage.css";
+
+function AllBeersPage() {
+  return (
+    <div id="AllBeers">
+      <div className="beer">
+        <img src="" height={240} width={160} alt="beer" />
+        <div className="description">
+          <h1>Buzz</h1>
+          <p>A Real Bitter Experience.</p>
+          <span>
+            <strong>Created by:</strong> Buzz
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 function HomePage() {
   // axios code
   return (
     <div id="Home">
-      <div className="category">
+      <Link to={"/beers"} className="category">
         <img src="public/beers.png" />
         <h1>All Beers</h1>
         <p>Lorem Ipsum</p>
-      </div>
+      </Link>
       <div className="category">
         <img src="random-beer.png" />
         <h1>Random Beer</h1>
