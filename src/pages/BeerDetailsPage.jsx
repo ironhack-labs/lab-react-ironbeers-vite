035ca83780c92aacc19ef0884ce76f70c9557fb4
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-07 12:40:38 +0200

    Iteration 5 with no returns

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,7 +1,22 @@
-
+import {useParams} from "react-router-dom"
+import axios from 'axios'
+import {useEffect, useState} from "react"
 
 function BeerDetailsPage() {
+    const {beerId} = useParams()
+    const [oneBeer, setOneBeer] = useState(null)
 
+    useEffect(()=> {
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+        .then((singleBeer)=> {
+            console.log(singleBeer)
+            setOneBeer(singleBeer)
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+
+    }, [beerId])
 
 }
 
