bf6e8c51746c2ed0e1a270af7b97bdadba990e97
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-07 17:33:56 +0200

    changes

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -4,28 +4,22 @@ import {useNavigate} from 'react-router-dom'
 
 function AddBeerPage() {
     
-    const [name, setName] = useState('')
-    const [tagline, setTagline] = useState('')
-    const [description, setDescription] = useState('')
-    const [first_brewd, setFirst_brewd] = useState('')
-    const [brewers_tips, setBrewers_tips] = useState('')
-    const [attenuation_level, setAttenuation_level] = useState(0)
-    const [contributed_by, setContributed_by] = useState('')
-
     const navigate = useNavigate()
     
 
+    const [newBeerCreated, setnewBeerCreated] = useState({})
 
   function handleSubmit() {
 
     e.preventDefault()
 
-    const {name, tagline, description, first_brewd, brewers_tips, attenuation_level, contributed_by} = newBeerCreated
+
+    
 
     
     
     axios
-      .post("https://ih-beers-api2.herokuapp.com/beers/new", newBeerCreated)
+      .post("https://ih-beers-api2.herokuapp.com/beers/new", newBeerCreated )
       .then(()=> {
 
         navigate('/beers')
