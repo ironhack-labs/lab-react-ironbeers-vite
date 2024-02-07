c935c02d605a1d16fae7e0350e44a6834b22c742
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2024-02-07 19:14:17 +0000

    finishing add form styles

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './AddBeerPage.css';
 
 function AddBeerPage() {
   const navigate = useNavigate();
@@ -32,63 +33,65 @@ function AddBeerPage() {
   }
 
   return (
-    <div>
-      <label>Name</label>
-      <input
-        type="text"
-        name="name"
-        value={newBeerCreated.name}
-        onChange={handleChange}
-      />
-      <label>Tagline</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>Tagline</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>Description</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>First Brewed</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>Brewers Tips</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>Attenuation Level</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
-      <label>Contributed By</label>
-      <input
-        type="text"
-        name="tagline"
-        value={newBeerCreated.tagline}
-        onChange={handleChange}
-      />
+    <div id="AddBeer">
+      <form id="add-beer">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={newBeerCreated.name}
+          onChange={handleChange}
+        />
+        <label>Tagline</label>
+        <textarea
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>Tagline</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>Description</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>First Brewed</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>Brewers Tips</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>Attenuation Level</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+        <label>Contributed By</label>
+        <input
+          type="text"
+          name="tagline"
+          value={newBeerCreated.tagline}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }
