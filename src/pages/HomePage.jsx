7d33b2d92eddc9d0e3ab29eaf98c13baf08e30a6
Eduardo Botelho <eduardo11224b@gmail.com>
2024-02-07 14:14:26 +0000

    homepage basic structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
+import "./HomePage.css";
+
 function HomePage() {
   // axios code
-  return <div id="HomePage"></div>;
+  return (
+    <div id="Home">
+      <div className="category">
+        <img src="public/beers.png" />
+        <h1>All Beers</h1>
+        <p>Lorem Ipsum</p>
+      </div>
+      <div className="category">
+        <img src="random-beer.png" />
+        <h1>Random Beer</h1>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
