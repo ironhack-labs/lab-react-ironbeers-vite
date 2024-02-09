17b4998c0aac2f75932bfbbe976dc676faa2ec80
Vladimir Ilovaca <vladoilovaca@gmail.com>
2024-02-09 23:39:11 +0100

    Iteration 5 done

--- src/services/beerService.js
+++ src/services/beerService.js
@@ -3,4 +3,4 @@ import httpBeers from "./baseBeerService";
 export const getBeers = () => httpBeers.get("/beers");
 
 export const getBeerDetail = (_id) => 
-httpBeers.delete(`/beers/${_id}`)
+httpBeers.get(`/beers/${_id}`)
