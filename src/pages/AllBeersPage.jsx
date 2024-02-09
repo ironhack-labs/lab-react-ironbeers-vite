4e6caef190b764fa328ccf0364836864c954635d
Vladimir Ilovaca <vladoilovaca@gmail.com>
2024-02-09 22:48:10 +0100

    Iteration 4 done

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getBeers } from '../services/beerService';
+import BeerCard from '../components/BeerCard';
+
+
 function AllBeersPage() {
+    const [beers, setBeers] = useState([])
+
+    useEffect(() => {
+        getBeers()
+            .then((beersApi) => {
+                setBeers(beersApi)
+            })
+            .catch((err) => console.error(err))
+    }, [])
 
     return(
-        <h1>All Beers</h1>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+            {beers.map((beer) => (
+                <div key={beer._id} className="col">
+                    <BeerCard {...beer}/>
+                </div>
+            ))}
+        </div>
     )
 }
 
--- src/services/beerService.js
+++ src/services/beerService.js
@@ -1,3 +1,6 @@
 import httpBeers from "./baseBeerService";
 
 export const getBeers = () => httpBeers.get("/beers");
+
+export const getBeerDetail = (_id) => 
+httpBeers.delete(`/beers/${_id}`)
