a5ab1fbcd7b72c248d1d20fd86b22bfd0316f673
Vladimir Ilovaca <vladoilovaca@gmail.com>
2024-02-11 15:07:16 +0100

    iteration 7 done

--- src/services/beerService.js
+++ src/services/beerService.js
@@ -5,4 +5,6 @@ export const getBeers = () => httpBeers.get("/beers");
 export const getBeerDetail = (_id) => 
 httpBeers.get(`/beers/${_id}`) 
 
-export const getRandomBeer = () => httpBeers.get("/beers/random")
+export const getRandomBeer = () => httpBeers.get("/beers/random") 
+
+export const createBeer = () => httpBeers.post("/beers/new")
