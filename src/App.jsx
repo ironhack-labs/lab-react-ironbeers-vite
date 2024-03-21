f8b1c5d7eceff16690052cd7e3834064953ce856
Idir FOUQUET <fouquetidir@gmail.com>
2024-03-21 20:05:44 +0100

    Iteration 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,16 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/beers" element={<AllBeersPage />} />
-        <Route path="/random-beer" element={<RandomBeerPage />} />
-        <Route path="/new-beer," element={<AddBeerPage />} />
-        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
-      </Routes>
+      <div className="container">
+        <h1>LAB | React IronBeers</h1>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/beers" element={<AllBeersPage />} />
+          <Route path="/random-beer" element={<RandomBeerPage />} />
+          <Route path="/new-beer," element={<AddBeerPage />} />
+          <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,48 @@
+import { Link } from "react-router-dom";
+import AllBeersPage from "./AllBeersPage";
+
 function HomePage() {
-    return (
-        <h1>Lol</h1>
-    )
+
+    const blockList = [
+        {
+            title: "All Beers",
+            url: "/src/assets/beers.png",
+            id: 1
+        },
+        {
+            title: "Random Beers",
+            url: "/src/assets/random-beer.png",
+            id: 2
+        },
+        {
+            title: "New Beer",
+            url: "/src/assets/new-beer.png",
+            id: 3
+        },
+    ]
+
+  return (
+    <div className="container-hp">
+      <h1>HomePage</h1>
+      { blockList.map((element) => {
+        return (<Link key={element.id} to={element.url}>
+        <div className="container-card">
+          <img src={element.url} alt="image" />
+          <div className="container-content">
+            <h2>{element.title}</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+              cum labore aliquid voluptatum dignissimos suscipit dicta, fugit
+              sequi, autem ab ullam? Minima aut a beatae rerum iure quod
+              pariatur at!
+            </p>
+          </div>
+        </div>
+      </Link>
+      )})}
+
+    </div>
+  );
 }
 
 export default HomePage;
