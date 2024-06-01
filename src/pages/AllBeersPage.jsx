ca345b87896c37ecfa7266b00b107ddb1984c561
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-06-01 14:31:37 +0100

    first-commit

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,44 @@
-function AllBeersPage() {}
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AllBeersPage() {
+    const [allBeers, setAllBeers] = useState([]);
+
+    const getAllBeers = async () => {
+        try {
+            const response = await axios.get('https://ih-beers-api2.herokuapp.com/beers'
+            );  
+            //console.log(response.data);
+            setAllBeers(response.data)
+        } catch (error) {
+            console.log('error fetching the projects', error)
+        }
+    };
+
+    useEffect(() => {
+        console.log('useEffect: Mounting');
+        getAllBeers();
+    }, []);
+
+    return (
+        <ul>
+            {allBeers.map(beer => {
+                
+                return (
+                    <li key={beer._id}>
+                        <img src={beer.image_url} />
+                        <Link to={`/beers/${beer._id}`}>
+                            <h1>{beer.name}</h1>
+                        </Link>
+                        <p>{beer.tagline}</p>
+                        <p>{beer.contributed_by}</p>
+                    </li>
+                )
+            })}
+            
+        </ul>
+    )
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,34 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+    return (
+        <div>
+            <img src="src/assets/beers.png" />
+            <Link to="/beers">
+                <h1>All Beers</h1>
+            </Link>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore est earum, consectetur aspernatur ea nam veritatis sunt laudantium! Expedita quisquam aspernatur nesciunt unde minima velit similique culpa temporibus dolores.
+            </p>
+
+            <img src="src/assets/random-beer.png" />
+            <Link to="/random-beer">
+                <h1>Random Beer</h1>
+            </Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laboriosam, eum maxime atque explicabo minus odit praesentium commodi libero beatae nesciunt enim id ut? Vel quaerat inventore voluptates eius aperiam.
+            </p>
+
+            <img src="src/assets/new-beer.png"/>
+            <Link to="/new-beer">
+                <h1>New Beer</h1>
+            </Link>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae accusamus consequuntur dolor qui accusantium culpa quisquam magnam, amet, tempore deleniti voluptate sapiente commodi modi excepturi facilis. Sequi vitae nemo cumque.
+            </p>
+
+        </div>
+    )
+}
 
 export default HomePage;
