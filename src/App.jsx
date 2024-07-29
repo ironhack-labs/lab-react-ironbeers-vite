9257edfea0fe7cbe09a078ba1e71e0bac806ab12
elpezpr <gabrielpena@hotmail.com>
2024-07-29 13:47:57 -0400

    added #4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import AllBeersPage from "./pages/AllBeersPage";
 
 
 function App() {
@@ -10,10 +11,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/beers" element={<AddBeersPage />} />
-        <Route path="/random-beer" element={<RandomBeerPage />} />
-        <Route path="/new-beer" element={<AddBeerPage />} />
-        <Route path="/beers/:beerId" element={<BeerDetailsPage />} /> */}
+        <Route path="/beers" element={<AllBeersPage />} />
+        {/* <Route path="/random-beer" element={<RandomBeerPage />} /> */}
+        {/* <Route path="/new-beer" element={<AddBeerPage />} /> */}
+        {/* <Route path="/beers/:beerId" element={<BeerDetailsPage />} /> */}
       </Routes>
     </>
   );
