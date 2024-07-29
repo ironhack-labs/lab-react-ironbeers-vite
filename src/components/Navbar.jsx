8756f6ebb562d08f5de6fa887a26501e2c874377
elpezpr <gabrielpena@hotmail.com>
2024-07-29 16:51:56 -0400

    added home icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import homeIcon from '../assets/home-icon.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-link">
-        Home
+        <img src={homeIcon} alt="Home" className="navbar-icon" />
       </Link>
     </nav>
   );
