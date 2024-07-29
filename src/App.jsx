88d740e064f763ee24ac57d1c2646dc7be727718
elpezpr <gabrielpena@hotmail.com>
2024-07-29 13:30:52 -0400

    fisnished iteration-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <Routes>
-    <Route path="/" element={<HomePage />} />
-    {/* <Route path="/beers" element={<AddBeersPage />} />
-    <Route path="/random-beer" element={<RandomBeerPage />} />
-    <Route path="/new-beer" element={<AddBeerPage />} />
-    <Route path="/beers/:beerId" element={<BeerDetailsPage  />} /> */}
-  </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/beers" element={<AddBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} /> */}
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
-function Navbar() {}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-link">
+        Home
+      </Link>
+    </nav>
+  );
+};
 
 export default Navbar;
