3aed9b5c65b87c3e1096415f3885b87de81ef454
elpezpr <gabrielpena@hotmail.com>
2024-07-29 12:27:39 -0400

    added #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React IronBeers</h1>
-    </div>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/beers" element={<AddBeersPage />} />
+    <Route path="/random-beer" element={<RandomBeerPage />} />
+    <Route path="/new-beer" element={<AddBeerPage />} />
+    <Route path="/beers/:beerId" element={<BeerDetailsPage  />} />
+  </Routes>
   );
 }
 
