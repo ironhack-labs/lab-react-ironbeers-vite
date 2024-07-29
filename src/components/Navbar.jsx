82d07ae8b5f6d3be17a5129c050ac18ef6c80882
elpezpr <gabrielpena@hotmail.com>
2024-07-29 17:33:33 -0400

    cleaned up css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import homeIcon from '../assets/home-icon.png';
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './AllBeersPage.css';
+
 const AllBeersPage = () => {
   const [beers, setBeers] = useState([]);
 
