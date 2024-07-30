d8f9b83f6d9f2e86a34fd42037508f16ea3e5eed
Ismail <ia.abousalem@gmail.com>
2024-07-29 22:39:23 -0400

    Pretty much finished lab

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,42 @@
-function BeerDetailsPage() {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './BeerDetailsPage.css';
+
+function BeerDetailsPage() {
+  const { beerId } = useParams();
+  const [beer, setBeer] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Log the response data to visualize the structure
+        setBeer(data);
+      })
+      .catch((error) => console.log(error));
+  }, [beerId]);
+
+  if (!beer) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="beer-details-page">
+      <img className="img" src={beer.image_url} alt={beer.name} />
+      <div className="details-container">
+        <div className="name-attenuation">
+          <h1>{beer.name}</h1>
+          <p> {beer.attenuation_level}</p>
+        </div>
+        <div className="tagline-first-brewed">
+          <h2 className="tagline">{beer.tagline}</h2>
+          <p> {beer.first_brewed}</p>
+        </div>
+        <p className="description">{beer.description}</p>
+        <p><strong>Contributed by:</strong> {beer.contributed_by.split(" <")[0]}</p>
+      </div>
+    </div>
+  );
+}
 
 export default BeerDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,36 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <img src="src\assets\beers.png" alt="" />
+      <Link className="link" to="/beers">
+        <h2>All Beers</h2>
+      </Link>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nesciunt facere quis dicta minus praesentium!
+      </p>
+      
+      <img src="src\assets\random-beer.png" alt="" />
+      <Link className="link" to="/random-beer">
+        <h2>Random Beer</h2>
+      </Link>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nisi autem beatae impedit odit iste.
+      </p>
+
+      <img src="src\assets\new-beer.png" alt="" />
+      <Link className="link" to="/new-beer">
+        <h2>New Beer</h2>
+      </Link>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ratione, quae molestias laborum iusto fuga.
+      </p>
+    </>
+  );
+}
 
 export default HomePage;
