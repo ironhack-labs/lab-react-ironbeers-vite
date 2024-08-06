cdf66cb478a40d2ffc1fb53796bb9091e0ebf672
rr1583 <rr1583@gmail.com>
2024-08-06 04:21:46 -0400

    done

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,3 +1,110 @@
-function AddBeerPage() {}
+import { useState } from "react";
+import axios from "axios";
+
+function AddBeerPage() {
+  const [formState, setFormState] = useState({
+    name: "",
+    tagline: "",
+    description: "",
+    first_brewed: "",
+    brewers_tips: "",
+    attenuation_level: 0,
+    contributed_by: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://ih-beers-api2.herokuapp.com/beers/new", formState)
+      .then((response) => {
+        console.log(response.data);
+        setFormState({
+          name: "",
+          tagline: "",
+          description: "",
+          first_brewed: "",
+          brewers_tips: "",
+          attenuation_level: 0,
+          contributed_by: "",
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="AddBeerPage">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Tagline:
+          <input
+            type="text"
+            name="tagline"
+            value={formState.tagline}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Description:
+          <textarea
+            name="description"
+            value={formState.description}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          First Brewed:
+          <input
+            type="text"
+            name="first_brewed"
+            value={formState.first_brewed}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Brewer's Tips:
+          <input
+            type="text"
+            name="brewers_tips"
+            value={formState.brewers_tips}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Attenuation Level:
+          <input
+            type="number"
+            name="attenuation_level"
+            value={formState.attenuation_level}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Contributed By:
+          <input
+            type="text"
+            name="contributed_by"
+            value={formState.contributed_by}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Add Beer</button>
+      </form>
+    </div>
+  );
+}
 
 export default AddBeerPage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,31 @@
-function RandomBeersPage() {}
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default RandomBeersPage;
+function RandomBeerPage() {
+  const [beer, setBeer] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers/random")
+      .then((response) => setBeer(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (!beer) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="RandomBeerPage">
+      <img src={beer.image_url} alt={beer.name} />
+      <h2>{beer.name}</h2>
+      <p>{beer.tagline}</p>
+      <p><b>First Brewed:</b> {beer.first_brewed}</p>
+      <p><b>Attenuation Level:</b> {beer.attenuation_level}</p>
+      <p>{beer.description}</p>
+      <p><b>Contributed by:</b> {beer.contributed_by}</p>
+    </div>
+  );
+}
+
+export default RandomBeerPage;
