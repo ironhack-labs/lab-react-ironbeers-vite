35c74b6df4b59c4eaa2833c2cdbe3556ff6d5446
Juan Manuel <j-grafi@hotmail.com>
2025-01-25 23:40:15 +0100

    random beer

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,51 @@
-function RandomBeersPage() {}
+
+import { useEffect,useState } from "react";
+import { listBeers } from "../services/BeersService";
+
+
+function RandomBeersPage() {
+    const [beer, setBeer] = useState(null)
+    
+    
+        useEffect(()=>{
+            listBeers()
+             .then(response =>{
+                const randomIndex = Math.floor(Math.random() * response.length);
+                setBeer(response[randomIndex]);
+             } 
+            )
+             .catch((err) => console.error(err))
+        },[])
+    return(
+        <>
+    {beer ? (
+        <div className="card d-flex flex-column col-md-12 col-sm-10 col-lg-8 align-items-center m-3">
+     
+            <img
+            className="card-img-top align-items-center"
+            style={{ width: "200px" }}
+            src={beer.image_url}
+            alt="Card image cap"/>
+            
+            <div className="card-body">
+            <div className="d-flex flex-row justify-content-between">
+                <h5 className="card-title">{beer.name}</h5>
+                <p>{beer.attenuation_level}</p>
+            </div>
+            <div className="d-flex flex-row justify-content-between">
+            <h6 className="card-title">{beer.tagline}</h6>
+            <p>{beer.first_brewed}</p>
+            </div>
+            <p>{beer.description}</p>
+            <p className="card-text">
+                contributed_by: {beer.contributed_by.split(" <")[0]}
+            </p>
+            </div>
+        </div>
+    ) : ( <p>loading...</p> )}
+    </>
+   
+    )
+}
 
 export default RandomBeersPage;
--- src/services/BeersService.js
+++ src/services/BeersService.js
@@ -3,5 +3,7 @@ import { createHttp } from "./baseService";
 const http = createHttp()
 
 export const listBeers = () => http.get('/beers')
-
+listBeers
 export const getBeer = (id) => http.get(`/beers/${id}`)
+
+
